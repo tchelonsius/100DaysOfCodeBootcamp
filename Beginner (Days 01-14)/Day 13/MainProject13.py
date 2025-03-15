f7ad97f0c61d44ebc778a461d1244d04366ab9d7
go73wax <go73wax@mytum.de>
2025-03-15 16:21:10 -0300

    Projects and exercises from day 13.

---
+++ Beginner (Days 01-14)/Day 13/MainProject13.py
@@ -0,0 +1,5 @@
+# Day 13: Debugging and Try-Except
+# Today's exercises were rather about reading python codes
+# and finding exceptions, logical errors and syntax mistakes
+# in the programs, and also learning about how to use the
+# debugger.
