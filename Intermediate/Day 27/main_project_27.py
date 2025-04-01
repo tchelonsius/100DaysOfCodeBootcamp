2d1bc3f3a5546fb770bac87f3027cf1f9775dcd9
go73wax <go73wax@mytum.de>
2025-04-01 16:34:30 -0300

    Day 27: GUI with Tkinter. Main Project: miles to km converter.

--- Intermediate/Day 27/main_project_27.py
+++ Intermediate/Day 27/main_project_27.py
@@ -4,16 +4,15 @@ def convert(event=None):
     result = float(entry.get())*1.609
     result_label["text"]=str(result)
 
-    
 window = tk.Tk()
 window.title("Mile to Km Converter")
-# window.minsize(width=300,height=200)
 window.config(padx=30,pady=30)
 
-
+# Entry in miles
 entry = tk.Entry(width=10)
 entry.grid(column=1, row=0)
 
+# Labels
 miles_label = tk.Label(text="Miles", font=("Arial", 20))
 miles_label.grid(column=2, row=0)
 
@@ -26,6 +25,7 @@ result_label.grid(column=1, row=1)
 km_label = tk.Label(text="Km", font=("Arial", 20))
 km_label.grid(column=2, row=1)
 
+# Button
 calculate_button = tk.Button(text="Calculate",command=convert)
 calculate_button.grid(column=1, row=2)
 window.bind("<Return>", convert)
